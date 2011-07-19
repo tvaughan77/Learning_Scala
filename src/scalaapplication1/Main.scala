a28168867057ff21a564565bc886d532f0aeee8b
Tom Vaughan <tom_vaughan@hotmail.com>
2011-07-19 12:42:23 -0400

    playing with scala.List

--- src/scalaapplication1/Main.scala
+++ src/scalaapplication1/Main.scala
@@ -9,5 +9,16 @@ object Main {
    */
   def main(args: Array[String]): Unit = {
     greetStrings.foreach(print)
+  
+    // This is a scala.List and it is immutable
+    val oneTwoThree = List(1, 2, 3)
+  
+    val oneTwo = List(1, 2)
+    val threeFour = List(3, 4)
+    val oneTwoThreeFour = oneTwo ::: threeFour
+  
+    println("" + oneTwo + " and " + threeFour + " were not mutated.")
+    println("Thus, " + oneTwoThreeFour + " is a new list.")
+  
   }
 }
