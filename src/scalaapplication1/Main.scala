67a96a41dfa110738a542286abbb038c3541b233
Tom Vaughan <tom_vaughan@hotmail.com>
2011-07-19 12:31:07 -0400

    simplifying greetString array construction and using foreach on the array

--- src/scalaapplication1/Main.scala
+++ src/scalaapplication1/Main.scala
@@ -2,19 +2,12 @@ package scalaapplication1
 
 object Main {
 
-  val greetStrings = new Array[String](3)
-  greetStrings.update(0, "Hello")
-  greetStrings(1) = ", "
-  greetStrings(2) = "World"
+  val greetStrings = Array("Hello", ", ", "World!")
   
   /**
    * @param args the command line arguments
    */
   def main(args: Array[String]): Unit = {
-    for(i <- (0).to(2)) 
-      print(greetStrings.apply(i))
-    
-    val three = (1).+(2)
-    println(three)
+    greetStrings.foreach(print)
   }
 }
