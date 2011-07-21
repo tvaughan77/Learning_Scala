700d58ab72b027b5fdec11b390bbc973d32e0289
Tom Vaughan <tom_vaughan@hotmail.com>
2011-07-21 10:54:15 -0400

    inlining and remove return statements to make code more concise

--- src/scalaapplication1/ch04/ChecksumAccumulator.scala
+++ src/scalaapplication1/ch04/ChecksumAccumulator.scala
@@ -5,11 +5,7 @@ class ChecksumAccumulator {
   // with no modifier, Scala defaults to public
   private var sum = 0
   
-  def add(b : Byte) : Unit = {
-    sum += b
-  }
+  def add(b : Byte) : Unit = sum += b
   
-  def checksum() : Int = {
-    return ~(sum & 0xFF) + 1
-  }
+  def checksum() : Int = ~(sum & 0xFF) + 1
 }
