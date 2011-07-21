91315fd65c61ce2ae94905af87ad3e69bdcb24d1
Tom Vaughan <tom_vaughan@hotmail.com>
2011-07-21 10:53:44 -0400

    naive and verbose method definitions

--- src/scalaapplication1/ch04/ChecksumAccumulator.scala
+++ src/scalaapplication1/ch04/ChecksumAccumulator.scala
@@ -4,4 +4,12 @@ class ChecksumAccumulator {
 
   // with no modifier, Scala defaults to public
   private var sum = 0
+  
+  def add(b : Byte) : Unit = {
+    sum += b
+  }
+  
+  def checksum() : Int = {
+    return ~(sum & 0xFF) + 1
+  }
 }
