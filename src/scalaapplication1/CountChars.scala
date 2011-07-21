a637722bfeba02aca83c9bdf75f48893722191f9
Tom Vaughan <tom_vaughan@hotmail.com>
2011-07-21 10:23:57 -0400

    returning to the example on page 55/56 with an actual printout in the middle of the stream processing

--- src/scalaapplication1/CountChars.scala
+++ src/scalaapplication1/CountChars.scala
@@ -7,8 +7,10 @@ class CountChars {
   def imperativeCountLines(args: Array[String]) : Int = {
     var lines = 0
     if(args.length > 0) {
-      for(line <- Source.fromFile(args(0)).getLines)
-          lines += 1
+      for(line <- Source.fromFile(args(0)).getLines) {
+        print(line.length + " " + line + "\n")
+        lines += 1
+      }
     }
     return lines
   }
