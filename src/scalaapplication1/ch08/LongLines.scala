8b0078a7043e58963bc054b75e826356eeae5923
tom.vaughan <tom_vaughan@hotmail.com>
2012-01-21 12:55:23 -0500

    Noting that you can simplify the wrapped helper method by making use of its visibility into its wrapper method's parameters

--- src/scalaapplication1/ch08/LongLines.scala
+++ src/scalaapplication1/ch08/LongLines.scala
@@ -10,9 +10,9 @@ object LongLines {
   def processFile(filename: String, width: Int) {
     val source = Source.fromFile(filename)
     for(line <- source.getLines) 
-      processLine(filename, width, line)
+      processLine(line)
     
-    def processLine(filename: String, width: Int, line: String) {
+    def processLine(line: String) {
       if(line.length > width) 
         println(filename + ": " + line.trim)
     }
